5ffe5c3400d0a8e28d18674f462359222040b423
masindes <maasinde.sylvester@gmail.com>
2024-09-29 16:04:26 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+function appendCat(name) {
+  const newArr = [...cats];
+  newArr.push(name);
+  return newArr;
+}
+function prependCat(name) {
+  const newArr = [...cats];
+  newArr.unshift(name);
+  return newArr;
+}
+function removeLastCat() {
+  const newArr = [...cats];
+  newArr.pop();
+  return newArr;
+}
+function removeFirstCat() {
+  const newArr = [...cats];
+  newArr.shift();
+  return newArr;
+}
